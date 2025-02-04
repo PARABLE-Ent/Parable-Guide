f2916a12aa00f562aa723811b3186efe7d7babd3
rudtjq640 <96940180+rudtjq640@users.noreply.github.com>
2025-02-04 09:49:56 +0900

    Update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -142,7 +142,7 @@ const config = {
         //     ],
         //   },
         // ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Parable Ent. 3D Style Guide Document. Built with Docusaurus.`,
       },
       prism: {
         theme: prismThemes.github,
